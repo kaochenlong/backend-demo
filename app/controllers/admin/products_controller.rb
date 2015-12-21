ee674b6e817b0f51c269af202975316faa43042a
Eddie Kao <eddie@digik.com.tw>
2015-12-21 20:58:43 +0800

    add devise and admin/base controller

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,7 +1,5 @@
-class Admin::ProductsController < ApplicationController
+class Admin::ProductsController < Admin::BaseController
   before_action :set_product, only: [:show, :edit, :update, :destroy]
-
-  layout 'admin'
 
   def index
     @products = Product.all
